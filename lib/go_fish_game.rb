3984a041d32d333f54929548f657dbd6806b47df
Ben Smith <ben@smithsrus.com>
2021-06-07 10:12:44 -0400

    add method to deal starting cards

--- lib/go_fish_game.rb
+++ lib/go_fish_game.rb
@@ -1,7 +1,20 @@
-class GoFishGame
-  attr_reader :players
+require_relative 'go_fish_player'
+require_relative 'shuffling_deck'
 
-  def start(players = ['player 1', 'player 2'])
+class GoFishGame
+  attr_reader :players, :deck
+
+  STARTING_CARD_COUNT = 5
+
+  def start(players = [GoFishPlayer.new(name: 'Player 1'), GoFishPlayer.new(name: 'Player 2')])
     @players = players
+    @deck = ShufflingDeck.new
+    deal_starting_cards()
+  end
+
+  def deal_starting_cards
+    players.each do |player|
+      STARTING_CARD_COUNT.times { player.pick_up_card(deck.deal) }
+    end
   end
 end
--- spec/go_fish_game_spec.rb
+++ spec/go_fish_game_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/go_fish_game'
+require_relative '../lib/go_fish_player'
 
 describe 'GoFishGame' do
   let(:game) { GoFishGame.new }
@@ -10,8 +11,16 @@ describe 'GoFishGame' do
     end
 
     it 'can have more than 2 players' do
-      game.start(['player 1', 'player 2', 'player 3'])
+      game.start([GoFishPlayer.new, GoFishPlayer.new, GoFishPlayer.new])
       expect(game.players.count).to eq 3
+    end
+
+    it 'deals cards for a 2 player game' do
+      game.start
+      player_1_card_count = game.players[0].card_count
+      player_2_card_count = game.players[0].card_count
+      expect(player_1_card_count).to eq GoFishGame::STARTING_CARD_COUNT
+      expect(player_2_card_count).to eq GoFishGame::STARTING_CARD_COUNT
     end
   end
 end
