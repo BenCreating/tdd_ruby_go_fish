cff9e19c56c53b20b5d95aeaa359dfcd27a672c6
Ben Smith <ben@smithsrus.com>
2021-06-07 12:07:39 -0400

    fix shuffling spec always passing

--- spec/shuffling_deck_spec.rb
+++ spec/shuffling_deck_spec.rb
@@ -1,19 +1,24 @@
 require_relative '../lib/shuffling_deck'
 
 describe 'ShufflingDeck' do
+  let(:deck) { ShufflingDeck.new }
+
+  def get_all_deck_card_ranks(deck)
+    card_ranks = []
+    while deck.cards_left > 0
+      card_ranks << deck.deal.rank
+    end
+    card_ranks
+  end
+
   it 'should shuffle the deck' do
     # There is a tiny chance that a shuffled deck will equal an unshuffled one
-    deck = ShufflingDeck.new
-    deck_shuffled = deck
+    deck_shuffled = ShufflingDeck.new
     deck_shuffled.shuffle
-
-    shuffled_ranks = []
-    unshuffled_ranks = []
-    while deck.cards_left > 0
-      unshuffled_ranks << deck.deal
-      shuffled_ranks << deck_shuffled.deal
-    end
-
-    expect(shuffled_ranks).not_to eq unshuffled_ranks
+    shuffled_card_ranks = get_all_deck_card_ranks(deck_shuffled)
+    unshuffled_card_ranks = get_all_deck_card_ranks(deck)
+    expect(shuffled_card_ranks.count).to eq 52
+    expect(unshuffled_card_ranks.count).to eq 52
+    expect(shuffled_card_ranks).not_to eq unshuffled_card_ranks
   end
 end
