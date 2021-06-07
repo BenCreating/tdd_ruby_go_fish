0d919ccee0ea01887847c2f62036dcc62a4da9f3
Ben Smith <ben@smithsrus.com>
2021-06-07 10:16:14 -0400

    fix starting card tests all looking at player 1

--- spec/go_fish_game_spec.rb
+++ spec/go_fish_game_spec.rb
@@ -18,16 +18,16 @@ describe 'GoFishGame' do
     it 'deals cards for a 2 player game' do
       game.start
       player_1_card_count = game.players[0].card_count
-      player_2_card_count = game.players[0].card_count
+      player_2_card_count = game.players[1].card_count
       expect(player_1_card_count).to eq GoFishGame::STARTING_CARD_COUNT
       expect(player_2_card_count).to eq GoFishGame::STARTING_CARD_COUNT
     end
 
     it 'deals cards for a 3 player game' do
-      game.start
+      game.start([GoFishPlayer.new, GoFishPlayer.new, GoFishPlayer.new])
       player_1_card_count = game.players[0].card_count
-      player_2_card_count = game.players[0].card_count
-      player_3_card_count = game.players[0].card_count
+      player_2_card_count = game.players[1].card_count
+      player_3_card_count = game.players[2].card_count
       expect(player_1_card_count).to eq GoFishGame::STARTING_CARD_COUNT
       expect(player_2_card_count).to eq GoFishGame::STARTING_CARD_COUNT
       expect(player_3_card_count).to eq GoFishGame::STARTING_CARD_COUNT
