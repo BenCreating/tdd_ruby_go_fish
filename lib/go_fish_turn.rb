cf8605187ff6c2e899f3aa938e2ca7f904f716cc
Ben Smith <ben@smithsrus.com>
2021-06-07 13:44:08 -0400

    initialize a turn

--- lib/go_fish_turn.rb
+++ lib/go_fish_turn.rb
@@ -1,4 +1,11 @@
 class GoFishTurn
+  attr_reader :turn_player
+
+  def initialize(turn_player, all_players)
+    @turn_player = turn_player
+    @all_players = all_players
+  end
+
   # def play_round
   #   table_cards = play_all_round_cards()
   #   self.last_round_table_cards = table_cards
