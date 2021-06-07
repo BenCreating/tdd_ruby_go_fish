483cf45a2c608e8d6ec60a23ec17e5e10655c378
Ben Smith <ben@smithsrus.com>
2021-06-07 10:15:31 -0400

    add test for dealing starting cards for a 3 player game

--- spec/go_fish_game_spec.rb
+++ spec/go_fish_game_spec.rb
@@ -22,5 +22,15 @@ describe 'GoFishGame' do
       expect(player_1_card_count).to eq GoFishGame::STARTING_CARD_COUNT
       expect(player_2_card_count).to eq GoFishGame::STARTING_CARD_COUNT
     end
+
+    it 'deals cards for a 3 player game' do
+      game.start
+      player_1_card_count = game.players[0].card_count
+      player_2_card_count = game.players[0].card_count
+      player_3_card_count = game.players[0].card_count
+      expect(player_1_card_count).to eq GoFishGame::STARTING_CARD_COUNT
+      expect(player_2_card_count).to eq GoFishGame::STARTING_CARD_COUNT
+      expect(player_3_card_count).to eq GoFishGame::STARTING_CARD_COUNT
+    end
   end
 end
