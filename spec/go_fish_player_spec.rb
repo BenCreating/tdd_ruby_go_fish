56accf34acf22d83344bb73a155a16920e10b67a
Ben Smith <ben@smithsrus.com>
2021-06-07 13:48:14 -0400

    clean up let(:player)

--- spec/go_fish_player_spec.rb
+++ spec/go_fish_player_spec.rb
@@ -25,7 +25,7 @@ describe 'GoFishPlayer' do
   end
 
   context '#give_cards' do
-    let(:player) { player = GoFishPlayer.new(cards: CardDeck.new([PlayingCard.new('2'), PlayingCard.new('J'), PlayingCard.new('3')])) }
+    let(:player) { GoFishPlayer.new(cards: CardDeck.new([PlayingCard.new('2'), PlayingCard.new('J'), PlayingCard.new('3')])) }
     it 'returns an array with 1 card when the player only has one card of a specified rank' do
       cards = player.give_cards_by_rank('2')
       expect(cards.count).to eq 1
