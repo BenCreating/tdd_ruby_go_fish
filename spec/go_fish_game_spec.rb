1405ee50a4ffb262381590484f1212dd0bd48f57
Ben Smith <ben@smithsrus.com>
2021-06-08 11:30:41 -0400

    fix game dealing tests looking for too many cards

--- spec/go_fish_game_spec.rb
+++ spec/go_fish_game_spec.rb
@@ -21,21 +21,16 @@ describe 'GoFishGame' do
     it 'deals cards for a 2 player game' do
       game = GoFishGame.new
       game.start
-      player_1_card_count = game.players[0].card_count
-      player_2_card_count = game.players[1].card_count
-      expect(player_1_card_count).to eq GoFishGame::STARTING_CARD_COUNT
-      expect(player_2_card_count).to eq GoFishGame::STARTING_CARD_COUNT
+      expect(game.players[0].card_count).to eq GoFishGame::STARTING_CARD_COUNT
+      expect(game.players[1].card_count).to eq GoFishGame::STARTING_CARD_COUNT
     end
 
     it 'deals cards for a 3 player game' do
       game = GoFishGame.new([GoFishPlayer.new, GoFishPlayer.new, GoFishPlayer.new])
       game.start
-      player_1_card_count = game.players[0].card_count
-      player_2_card_count = game.players[1].card_count
-      player_3_card_count = game.players[2].card_count
-      expect(player_1_card_count).to eq GoFishGame::STARTING_CARD_COUNT
-      expect(player_2_card_count).to eq GoFishGame::STARTING_CARD_COUNT
-      expect(player_3_card_count).to eq GoFishGame::STARTING_CARD_COUNT
+      expect(game.players[0].card_count).to eq GoFishGame::STARTING_CARD_COUNT
+      expect(game.players[1].card_count).to eq GoFishGame::STARTING_CARD_COUNT
+      expect(game.players[2].card_count).to eq GoFishGame::STARTING_CARD_COUNT
     end
   end
 
@@ -43,16 +38,16 @@ describe 'GoFishGame' do
     it 'deals cards to 2 players' do
       game = GoFishGame.new([GoFishPlayer.new, GoFishPlayer.new])
       game.deal_starting_cards
-      expect(game.players[0].card_count).to eq GoFishGame::STARTING_CARD_COUNT * 2
-      expect(game.players[1].card_count).to eq GoFishGame::STARTING_CARD_COUNT * 2
+      expect(game.players[0].card_count).to eq GoFishGame::STARTING_CARD_COUNT
+      expect(game.players[1].card_count).to eq GoFishGame::STARTING_CARD_COUNT
     end
 
     it 'deals cards to 3 players' do
       game = GoFishGame.new([GoFishPlayer.new, GoFishPlayer.new, GoFishPlayer.new])
       game.deal_starting_cards
-      expect(game.players[0].card_count).to eq GoFishGame::STARTING_CARD_COUNT * 2
-      expect(game.players[1].card_count).to eq GoFishGame::STARTING_CARD_COUNT * 2
-      expect(game.players[2].card_count).to eq GoFishGame::STARTING_CARD_COUNT * 2
+      expect(game.players[0].card_count).to eq GoFishGame::STARTING_CARD_COUNT
+      expect(game.players[1].card_count).to eq GoFishGame::STARTING_CARD_COUNT
+      expect(game.players[2].card_count).to eq GoFishGame::STARTING_CARD_COUNT
     end
   end
 
