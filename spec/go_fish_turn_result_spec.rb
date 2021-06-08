666ed757dc60669fd6926e7922a853cf4feda8f4
Ben Smith <ben@smithsrus.com>
2021-06-08 16:07:55 -0400

    part way through turn result

---
+++ spec/go_fish_turn_result_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/go_fish_turn_result.rb'
+
+class MockGoFishPlayer
+  attr_reader :name
+  def initialize(name == 'Henry')
+    @name = name
+  end
+end
+
+describe 'GoFishTurnResult' do
+  it 'generates a description of a player winning a card' do
+
+  end
+end
