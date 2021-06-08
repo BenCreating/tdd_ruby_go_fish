a60a85539ad91cb8405d8f29b618a59e5e05fd23
Ben Smith <ben@smithsrus.com>
2021-06-07 20:16:03 -0400

    replace mock player with true player

--- spec/go_fish_turn_spec.rb
+++ spec/go_fish_turn_spec.rb
@@ -1,27 +1,7 @@
 require_relative '../lib/go_fish_turn'
 require_relative '../lib/player_hand'
 require_relative '../lib/playing_card'
-
-class MockGoFishPlayer
-  attr_reader :name, :cards
-
-  def initialize(name: 'Anonymous', cards: PlayerHand.new([]))
-    @name = name
-    @cards = cards
-  end
-
-  def pick_up_card(card)
-    cards.add_card(card)
-  end
-
-  def give_cards_by_rank(rank)
-    cards.take_cards_by_rank(rank)
-  end
-
-  def take_from(target_player, target_rank)
-    target_player.give_cards_by_rank(target_rank)
-  end
-end
+require_relative '../lib/go_fish_player'
 
 describe 'GoFishTurn' do
   let(:player_1_cards) { [PlayingCard.new('5'), PlayingCard.new('7')] }
