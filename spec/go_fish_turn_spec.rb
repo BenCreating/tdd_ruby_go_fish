b7d40f45cd6f1a70303df962ccb9854203a71fe3
Ben Smith <ben@smithsrus.com>
2021-06-07 20:16:18 -0400

    add third player to tests

--- spec/go_fish_turn_spec.rb
+++ spec/go_fish_turn_spec.rb
@@ -4,9 +4,10 @@ require_relative '../lib/playing_card'
 require_relative '../lib/go_fish_player'
 
 describe 'GoFishTurn' do
-  let(:player_1_cards) { [PlayingCard.new('5'), PlayingCard.new('7')] }
-  let(:player_2_cards) { [PlayingCard.new('K'), PlayingCard.new('2')] }
-  let(:players) { [MockGoFishPlayer.new(cards: PlayerHand.new(player_1_cards)), MockGoFishPlayer.new(cards: PlayerHand.new(player_2_cards))] }
+  let(:player_1_cards) { [PlayingCard.new('K'), PlayingCard.new('7'), PlayingCard.new('7'), PlayingCard.new('7')] }
+  let(:player_2_cards) { [PlayingCard.new('K'), PlayingCard.new('2'), PlayingCard.new('7')] }
+  let(:player_3_cards) { [PlayingCard.new('K'), PlayingCard.new('K')] }
+  let(:players) { [GoFishPlayer.new(cards: PlayerHand.new(player_1_cards)), GoFishPlayer.new(cards: PlayerHand.new(player_2_cards)), GoFishPlayer.new(cards: PlayerHand.new(player_3_cards))] }
 
 
 end
